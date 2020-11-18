94af6f0a47b5b597790556c91823a4dd49a641f8
Ahmed Gamal <agmghazi@hotmail.com>
2020-11-18 09:24:39 +0300

    use createSlice func

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,23 +1,24 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-//Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugUpdate = createAction("bugUpdate");
-export const DELETE_ADDED = createAction("DELETE_ADDED");
-
-//Reducer
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugUpdate.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index] = { resolved: true };
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugUpdate: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index] = { resolved: true };
+    },
   },
 });
+
+export const { bugAdded, bugUpdate } = slice.actions;
+export default slice.reducer;
