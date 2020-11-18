4eac4b74cbd7fca9ad8cd3d806cae3e29e6d7b15
Ahmed Gamal <agmghazi@hotmail.com>
2020-11-18 11:16:38 +0300

    add custome middleware like login

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import { projectAdded } from "./store/project";
 
 function App() {
   const store = configureAppStore();
+
+  //like get data from API
+  store.dispatch({
+    type: "error",
+    payload: { message: "An error occurred" },
+  });
+
   // const unsubscripe = store.subscribe(() => {
   //   console.log("update state", store.getState());
   // });
 
   store.dispatch(bugAdded({ description: "add patch 1" }));
-  store.dispatch(bugUpdate({ id: 1 }));
-  store.dispatch(projectAdded({ name: "project 1" }));
+  // store.dispatch(bugUpdate({ id: 1 }));
+  // store.dispatch(projectAdded({ name: "project 1" }));
 
-  const unresolveBugs = getUnresolvedBugs(store.getState());
-  const unresolveBugs2 = getUnresolvedBugs(store.getState());
-  console.log("unresolveBugs" + unresolveBugs);
-  console.log(unresolveBugs === unresolveBugs2);
+  // const unresolveBugs = getUnresolvedBugs(store.getState());
+  // const unresolveBugs2 = getUnresolvedBugs(store.getState());
+  // console.log("unresolveBugs" + unresolveBugs);
+  // console.log(unresolveBugs === unresolveBugs2);
 
-  // unsubscripe();
-  console.log(store);
-  console.log(store.getState());
+  // // unsubscripe();
+  // console.log(store);
+  // console.log(store.getState());
 
   return (
     <div className="App">
